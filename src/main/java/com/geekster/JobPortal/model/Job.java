088929123c63f0d8b1051eac7fc2d3ac885ecda2
Satyam <Satyamjaiswal08795@gmail.com>
2023-04-30 18:37:55 +0530

    JobSearchPortlH2database

---
+++ src/main/java/com/geekster/JobPortal/model/Job.java
@@ -0,0 +1,55 @@
+package com.geekster.JobPortal.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Job {
+    @NotNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "Title cannot be null")
+    @Length(min = 1, max = 255, message = "Title length should be between 1 and 255")
+    private String title;
+
+    @NotBlank(message = "Description cannot be null")
+   @Length(max = 500)
+    private String description;
+
+    @NotBlank
+    private String location;
+
+    @NotNull(message = "Salary cannot be null")
+    @PositiveOrZero(message = "Salary should be positive or zero")
+    private Double salary;
+
+    @NotBlank
+    private String companyName;
+
+    @NotBlank
+    private String employerName;
+
+    @NotNull(message = "Job type cannot be null")
+    @Enumerated(EnumType.STRING)
+    private JobType jobType;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+    private LocalDate appliedDate;
+}
